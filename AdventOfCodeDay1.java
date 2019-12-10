47796a165ad8be8ca9293ab63db5e58a36a264fa
Mahanth <mohan.mahanth@gmail.com>
2019-12-10 09:05:38 -0800

    Add files via upload
    This is the program for the Advent of Code day 1 challenge

---
+++ AdventOfCodeDay1.java
@@ -0,0 +1,54 @@
+import java.lang.Math;
+public class AdventOfCodeDay1 {
+
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int result,i1,i2,i3,i4,i5,i6,i7,r1,r2,r3,r4,r5,r6,r7;
+	int [] fuelarr1= {91617,134652,101448,83076,53032,80487,106061,103085,71513,143874,102830,121433,139937,104468};
+	int [] fuelarr2= {53098,75999,113915,73992,90028,64164,101248,111333,89201,89076,129360,81573,54381,64105,104272};
+	int [] fuelarr3= {144188,81022,125558,87910,135654,110929,131610,147160,139648,118129,93967,123117,77927,112034,84847};
+    int [] fuelarr4= {145527,72652,123043,136324,71228,118583,56992,141812,60119,105185,97653,134563,54195,64473,75606};
+    int [] fuelarr5= {148515,88765,112562,52156,119805,117149,149791,128964,108955,55806,86025,148350,74382,73632,141124};
+    int [] fuelarr6= {101688,106829,132594,113645,90320,104874,95210,118499,56445,86371,113833,122860,112507,55964,105993,92005};
+    int [] fuelarr7= {83760,90258,56238,127426,147641,129484,107162,99535,107975,136238};
+ r1=13;r2=14;r3=14;r4=14;r5=14;r6=15;r7=9; 
+for(i1=0;i1<=r1;i1++)
+{
+	result=Math.round((fuelarr1[i1]/3))-2;
+	System.out.println("The fuel required is "+ result);
+}
+for(i2=0;i2<=r2;i2++)
+{
+	result=Math.round((fuelarr1[i2]/3))-2;
+	System.out.println("The fuel required is "+ result);
+}
+for(i3=0;i3<=r3;i3++)
+{
+	result=Math.round((fuelarr1[i3]/3))-2;
+	System.out.println("The fuel required is "+ result);
+}
+for(i4=0;i4<=r4;i4++)
+{
+	result=Math.round((fuelarr1[i4]/3))-2;
+	System.out.println("The fuel required is "+ result);
+}
+for(i5=0;i5<=r5;i5++)
+{
+	result=Math.round((fuelarr1[i5]/3))-2;
+	System.out.println("The fuel required is "+ result);
+}
+for(i6=0;i6<=r6;i6++)
+{
+	result=Math.round((fuelarr1[i6]/3))-2;
+	System.out.println("The fuel required is "+ result);
+}
+for(i7=0;i7<=r7;i7++)
+{
+	result=Math.round((fuelarr1[i7]/3))-2;
+	System.out.println("The fuel required is "+ result);
+}
+
+
+	}
+
+}
