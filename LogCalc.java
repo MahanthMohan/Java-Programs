60630e858893ff60791fe526c3500122fd6e820e
Mahanth <mohan.mahanth@gmail.com>
2020-06-15 21:13:11 -0700

    fixes

--- LogCalc.java
+++ LogCalc.java
@@ -1,46 +1,51 @@
 import java.util.Scanner;
-import java.math.*;
-public class LogCalc {
-static double x,y,op;
-static Scanner keyboard=new Scanner(System.in);
-public void NaturalLog()
-{
-	System.out.println("Enter the value");
-	x=keyboard.nextDouble();
-	//System.out.println(Math.log(x));
-}
-public void Log()
-{
-	System.out.println("Enter the value");
-	x=keyboard.nextDouble();
-	//System.out.println(Math.log10(x));
-}
-public void e()
-{
-	System.out.println("Enter the power that e needs to be raised to");
-	x=keyboard.nextDouble();
-	System.out.println(Math.pow(2.71828, x));
-}
-static LogCalc Method=new LogCalc();
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	
-		System.out.println("What do you want to calculate - ln(1),log(2),and e(3)");
-		op=keyboard.nextDouble();
-		if(op==1) 
-		{
-			Method.NaturalLog();
-		}
-		else if(op==2) 
-		{
-			Method.Log();
-		}
-		else if(op==3) 
-		{
-			Method.e();
-		}
+import java.lang.Math;
 
+public class LogCalc {
+
+	static Double NaturalLog(Double value)
+	{
+		Double result = Math.log(value);
+		return result;
 	}
+
+	static Double Log(Double value)
+	{
+		Double result = Math.log10(value);
+		return result;
+	}
+
+	static Double e(Double value)
+	{
+		Double result = Math.pow(2.71828, value);
+		return result;
+	}
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			LogCalc logcalc = new LogCalc();
+			Scanner sc = new Scanner(System.in);
+			System.out.println("What do you want to calculate - ln, log, or e: ");
+			String op = sc.next();
+
+			if(op == "ln") 
+			{
+				Double value = sc.nextDouble();
+				System.out.println(logcalc.NaturalLog(value));
+			}
+			else if(op == "ln") 
+			{
+				Double value = sc.nextDouble();
+				System.out.println(logcalc.Log(value));
+			}
+			else if(op == "e") 
+			{
+				Double value = sc.nextDouble();
+				System.out.println(logcalc.e(value));
+			}
+			
+			sc.close();
+
+		}
 
 }
